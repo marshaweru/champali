3945764ca309cb43ef34fabae796a7a21b36db89
weru <weru.marsha@gmail.com>
2023-12-06 23:34:22 -0800

    dependencies for custom font,API and env variables

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-import { view, Text } from 'react-native'; //view as a div text as a p tag
+import { View, Text } from 'react-native'; //view as a div text as a p tag
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default Home;
