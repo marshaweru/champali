7b04a1130bbffd48581568964849f46c265a5ad3
weru <weru.marsha@gmail.com>
2023-12-09 00:20:25 -0800

    wrap with safeareaview to safely show content

--- app/index.js
+++ app/index.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'; //view as a div text as a p tag
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native'; //view as a div text as a p tag
+import { Stack, useRouter } from 'expo router';
+
+import { COLORS, icons, images, SIZES } from '../constants';
+import { 
+    Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome 
+} from '../components';
 
 const Home = () => {
+    //define router
+    const router = useRouter();
+
     return (
-        <View>
+        <SafeAreaView>
             <Text>Home</Text>
-        </View>
+        </SafeAreaView>
     )
 }
 
