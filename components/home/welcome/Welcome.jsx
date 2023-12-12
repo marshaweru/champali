508b654d27e91ef26d96632442807ba5b499cff0
weru <weru.marsha@gmail.com>
2023-12-11 23:06:52 -0800

    Finish up on the UI/UX of the app

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react' from 'react'
+import { useState } from 'react' 
 import {
   View,
   Text,
@@ -12,11 +12,56 @@ import { useRouter } from 'expo-router';
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants'
 
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
+
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] =useState('full-time')
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Weru</Text>
+        <Text style={styles.welcomeMessage}>Find yur perfect jobs</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchInput} 
+            value=""
+            onChange={() => {}}
+            placeholder="What are you looking for?"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tabsContainer}>
+      <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity 
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          )}
+          heyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+        />
+      </View>
     </View>
   )
 }
