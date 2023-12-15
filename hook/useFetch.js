24055097187deaaa79272a754cb3ce1f7e72dbcc
weru <weru.marsha@gmail.com>
2023-12-14 23:40:38 -0800

    new env file to store API key

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -9,14 +9,13 @@ const useFetch = (endpoint) => {
     const options = {
         method: 'GET',
         url: 'https://jsearch.p.rapidapi.com/${endpoint}',
+        headers: {
+            'X-RapidAPI-Key': '6afc9cf076msh82df439f7636919p151941jsn7352194bdc44',
+            'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+          },
         params: {
           query: 'Python developer in Texas, USA',
           page: '1',
-          num_pages: '1'
-        },
-        headers: {
-          'X-RapidAPI-Key': '6afc9cf076msh82df439f7636919p151941jsn7352194bdc44',
-          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
-        }
+          num_pages: '1'},
       };      
 }
