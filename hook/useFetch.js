38a61002d8f6d78b468664813c1f838ae1996d27
weru <weru.marsha@gmail.com>
2023-12-15 03:16:17 -0800

    Import API key from env file

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import { RAPID_API_KEY } from '@env';
 
 const useFetch = (endpoint) => {
     const [data, setData] = useState([]);
