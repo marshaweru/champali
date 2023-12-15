c74100a12fb50bf38d52da81b5bd2b2c80da5eec
weru <weru.marsha@gmail.com>
2023-12-15 03:33:12 -0800

    Fetch data

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -17,5 +17,32 @@ const useFetch = (endpoint, query) => {
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
           },
         params: { ...query },
-      };      
+      };
+
+      const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+          const response = await axios.request
+          (options);
+
+          setData(response.data.data);
+          setIsLoading(false);
+        } catch (error) {
+          setError(error);
+          alert('There is an error')
+        }finally {
+          setIsLoading(false);
+        }
+      }
+
+      useEffect(() => {
+        fetchData();
+      }, []);
+
+      const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+      }
+      return { data, isLoading, error, refetch };
 }
