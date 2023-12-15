0849f34b45e182a55edaca7a1d4708d1ebbf62ac
weru <weru.marsha@gmail.com>
2023-12-14 23:26:03 -0800

    Paste API code snippet from rapidAPI

---
+++ hook/useFetch.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+const useFetch = () => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        method: 'GET',
+        url: 'https://jsearch.p.rapidapi.com/search',
+        params: {
+          query: 'Python developer in Texas, USA',
+          page: '1',
+          num_pages: '1'
+        },
+        headers: {
+          'X-RapidAPI-Key': '6afc9cf076msh82df439f7636919p151941jsn7352194bdc44',
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        }
+      };      
+}
