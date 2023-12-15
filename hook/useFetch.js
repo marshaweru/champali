59b6ab1b5e4913f3153fc3446b6ce344e3847f5d
weru <weru.marsha@gmail.com>
2023-12-14 23:30:03 -0800

    Probe to use fetch hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
 
-const useFetch = () => {
+const useFetch = (endpoint) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
