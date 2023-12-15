8024c8f7b7a4c3d972264b57b0d2daaca090f029
weru <weru.marsha@gmail.com>
2023-12-15 03:20:13 -0800

    Pass a query to make it dynamic

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -4,7 +4,7 @@ import { RAPID_API_KEY } from '@env';
 
 const rapidApiKey = RAPID_API_KEY;
 
-const useFetch = (endpoint) => {
+const useFetch = (endpoint, query) => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
@@ -13,12 +13,9 @@ const useFetch = (endpoint) => {
         method: 'GET',
         url: 'https://jsearch.p.rapidapi.com/${endpoint}',
         headers: {
-            'X-RapidAPI-Key': '',
+            'X-RapidAPI-Key': rapidApiKey,
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
           },
-        params: {
-          query: 'Python developer in Texas, USA',
-          page: '1',
-          num_pages: '1'},
+        params: { ...query },
       };      
 }
